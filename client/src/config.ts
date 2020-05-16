e2d1cbd4e39d8a78597cfad94356e5f6f31899d9
Obisesan <aobisesan@deloitte.com.ng>
2020-05-17 00:24:52 +0100

    submission

---
+++ client/src/config.ts
@@ -0,0 +1,10 @@
+
+const apiId = '9hqyk4i64f'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  
+  domain: 'dev-nt0cz7ex.auth0.com',            // Auth0 domain
+  clientId: '0P5DavyV0jekDeJzSC2ROMT7Ue7Kqm70',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
