f2665b38bca982510c41a306e951c2d12b14e8b0
Obisesan <aobisesan@deloitte.com.ng>
2020-05-17 10:52:18 +0100

    fixed based on reviewer's comments

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 
-const apiId = '9hqyk4i64f'
+const apiId = 'e2mg7iqieb'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   
-  domain: 'dev-nt0cz7ex.auth0.com',            // Auth0 domain
-  clientId: '0P5DavyV0jekDeJzSC2ROMT7Ue7Kqm70',          // Auth0 client id
+  domain: 'dev-tk2m75mz.auth0.com',            // Auth0 domain
+  clientId: '0Z4ZocBelgfq4S85jyOJTxVv10h1wmr1',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
